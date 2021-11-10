fff0eb96f7c4a312988bc23668512320195519f8
Lucas-Ed <lucas_tranpo@YAHOO.com.br>
2021-11-10 00:30:34 -0300

    Atualização

---
+++ Game-Mania Final/js/scriptform.js
@@ -0,0 +1,57 @@
+function Verifica(){
+    var  Nome = document.getElementById("validationCustom01").value;
+    var Ultimonome = document.getElementById("validationCustom02").value;
+    var Email = document.getElementById("validationCustomUsername").value;
+    var Cidade = document.getElementById("validationCustom03").value;
+    var Estado = document.getElementById("validationCustom04").value;
+    var cep = document.getElementById("validationCustom05").value;
+    var  Check = document.getElementById("invalidCheck").checked;
+    if(Nome == ""){
+        alert("Preencha o campo Nome");
+        return false;
+    
+    }
+    else if(Ultimonome == ""){
+        alert("Preencha o campo Ultimo Nome");
+        return false;
+    }
+    else if(Email == ""){
+        alert("Preencha o campo Email");
+        return false;
+    }
+    else if(Cidade == ""){
+        alert("Preencha o campo Cidade");
+        return false;
+    }
+    else if(Estado == ""){
+        alert("Preencha o campo Estado");
+        return false;
+    }
+    else if(cep == ""){
+        alert("Preencha o campo Cep");
+        return false;
+    }
+    else if (Check == false){
+        alert("Aceite os termos");
+        return false;
+    }
+    else{
+        alert("Formulário enviado com sucesso");
+        return true;
+    }
+   
+
+
+}
+// != Negação
+//Atividade SA2 - Encontro Presencial 4
+//Ativar Botão Enviar formilário asomente quando todos os campos estiverem preenchidos.
+$('#validationCustom05, #validationCustom01, #forme').change(function(){
+    var cep = $("#validationCustom05").val();
+    var nome = $("#validationCustom01").val();
+    if(cep != null && nome != null){
+       $('.btn').removeAttr('disabled');
+}else{
+    $('.btn').attr('disabled');
+}
+})
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# UC9
-Repositório para cumprir o exercicíos de prototipagem de layout- UC9 do curso Front End Senai.
-A apresentação dos layouts podem ser vistas [aqui.](https://bit.ly/3EWTH8W)
+# UC9 do curso Front End Senai.
+Este repositório é para cumprir os exercicíos do curso do senai de Frontend, neles estão os 3 desafios do módulo 9 do curso, feitos por mim.
+A apresentação dos layouts da prototípagem podem ser vistas [aqui.](https://bit.ly/3EWTH8W)
+O projeto final pode ser visto [aqui:](https://bit.ly/3n0GcOX)
